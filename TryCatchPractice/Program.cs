e201b89340a6fca953ecf8f0b852c28116ab9864
NIHAT <NIHAT@DESKTOP-3F2LN5P>
2024-08-25 17:59:45 +0300

    Add project files.

---
+++ TryCatchPractice/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace TryCatchPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Bir sayı giriniz..."); 
+
+                try
+                {
+                    int number = Convert.ToInt32(Console.ReadLine()); // Kullanıcıdan sayı alıyoruz.
+                    Console.WriteLine($"Sayının karesi = {number*number}"); // Alınan değer int değer ise ekrana sayının karesi yazdırılır döngüden çıkış yapılır.
+                    break; // Doğru sonuç geldiğinde çıkış yapılır.
+                }
+
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Geçersiz giriş! Lütfen bir sayı giriniz."); // Kullanıcı hatalı değer girerse catch yakalar ve ekrana hatalı olduğu yazdırılır.
+                }
+                
+            }
+        }
+
+
+    }
+
+    
+
+}
